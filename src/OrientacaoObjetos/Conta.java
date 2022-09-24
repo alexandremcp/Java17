75130bab2ddacd3ab39d22be64d2cd0e8b6d5e87
Alexandre Luiz dos Santos <alexandremcp@gmail.com>
2022-09-24 11:30:36 -0300

    Orientação a Objetos - Classe Conta

---
+++ src/OrientacaoObjetos/Conta.java
@@ -0,0 +1,13 @@
+package OrientacaoObjetos;
+
+public class Conta {
+
+    public static void main(String[] args) {
+
+        int agencia;
+        int numero;
+        String titular;
+        double saldo;
+
+    }
+}
