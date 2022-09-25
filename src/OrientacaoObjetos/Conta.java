e1a1b83b55194305ec0b55c73033e09071f0ddb5
Alexandre Luiz dos Santos <alexandremcp@gmail.com>
2022-09-25 08:25:45 -0300

    Método com retorno

--- src/OrientacaoObjetos/Conta.java
+++ src/OrientacaoObjetos/Conta.java
@@ -10,4 +10,13 @@ public class Conta {
         void depositar(double valor) {
             this.saldo += valor;
         }
+
+        boolean sacar(double valor) {
+            if (this.saldo >= valor) {
+                this.saldo -= valor;
+                return true;
+            } else {
+                return false;
+            }
+        }
 }
--- src/OrientacaoObjetos/TestaMetodo.java
+++ src/OrientacaoObjetos/TestaMetodo.java
@@ -5,10 +5,16 @@ public class TestaMetodo {
     public static void main(String[] args) {
 
         Conta conta = new Conta();
-        conta.depositar(100);
-        conta.depositar(50);
-        conta.depositar(200);
-        System.out.println(conta.saldo);
+        conta.depositar(300);
+        conta.depositar(500);
+        conta.depositar(50.30);
+
+        boolean conseguiuRealizarSaque = conta.sacar(1000);
+        if (conseguiuRealizarSaque) {
+            System.out.println("Saque realizado com sucesso!");
+        } else {
+            System.out.println("Não foi possível realizar o saque!");
+        }
 
     }
 }
